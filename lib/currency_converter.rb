b9a679551de6f879d0563b16b9db838ed05b9f8a
Robert Luckfield <rluck0419@gmail.com>
2016-06-23 16:02:48 -0500

    test for basic conversions - can convert from USD to EUR and visa versa

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -4,4 +4,15 @@ class CurrencyConverter
   def initialize
     @codes = {USD: 1.0, EUR: 0.87}
   end
+
+  def convert(currency_obj, currency_code)
+    currency_sym = currency_code.to_sym
+    if currency_obj.type == "USD"
+      amount = currency_obj.amount * codes[currency_sym]
+    else
+      mul = 1 / codes[currency_obj.type.to_sym]
+      amount = currency_obj.amount * mul
+    end
+    Currency.new(amount.round, currency_code)
+  end
 end
--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -1,10 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/currency_converter.rb'
+require_relative '../lib/currency.rb'
 
 class CurrencyConverterTest < MiniTest::Test
   def currency_converter
     ::CurrencyConverter.new
+  end
+
+  def currency(amount, type)
+    ::Currency.new(amount, type)
   end
 
   def test_currency_converter_exists
@@ -14,5 +19,10 @@ class CurrencyConverterTest < MiniTest::Test
   def test_currency_converter_codes
     codes = {USD: 1.0, EUR: 0.87}
     assert_equal currency_converter.codes, codes
+  end
+
+  def test_currency_convert
+    assert_equal currency_converter.convert(currency(100,"USD"), "EUR"), currency(87,"EUR")
+    assert_equal currency_converter.convert(currency(87,"EUR"), "USD"), currency(100,"USD")
   end
 end
