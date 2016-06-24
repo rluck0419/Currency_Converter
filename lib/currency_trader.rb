537e61ca41bf9ea0e9200bc3338790cbb270f5f3
Robert Luckfield <rluck0419@gmail.com>
2016-06-24 12:45:31 -0500

    minor refactoring - constant for codes hash in currency_converter_test.rb

--- lib/currency_trader.rb
+++ lib/currency_trader.rb
@@ -1,5 +1,13 @@
 class CurrencyTrader
+  # conversion rates at time 1
+  CODES_ONE = {USD: 1.0, EUR: 0.88, GBP: 0.67, JPY: 102.20}
+  # conversion rates at time 2
+  CODES_TWO = {USD: 1.0, EUR: 0.89, GBP: 0.72, JPY: 102.20}
   def initialize(first_converter, second_converter, starting_currency = "USD")
-    
+    @first_converter = CODES_ONE
+    @second_converter = CODES_TWO
+    @starting_currency = starting_currency
   end
+
+  
 end
--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -4,6 +4,7 @@ require_relative '../lib/currency_converter.rb'
 require_relative '../lib/currency.rb'
 
 class CurrencyConverterTest < MiniTest::Test
+  CODES = {USD: 1.0, EUR: 0.88, GBP: 0.67}
   def currency_converter(codes)
     ::CurrencyConverter.new(codes)
   end
@@ -13,32 +14,27 @@ class CurrencyConverterTest < MiniTest::Test
   end
 
   def test_currency_converter_exists
-    codes = {USD: 1.0, EUR: 0.88}
-    assert currency_converter(codes)
+    assert currency_converter(CODES)
   end
 
   def test_currency_converter_codes
-    codes = {USD: 1.0, EUR: 0.88}
-    assert_equal currency_converter(codes).codes, codes
+    assert_equal currency_converter(CODES).codes, CODES
   end
 
   def test_currency_convert_two
-    codes = {USD: 1.0, EUR: 0.88}
-    assert_equal currency_converter(codes).convert(currency(100, "USD"), "USD"), currency(100, "USD")
-    assert_equal currency_converter(codes).convert(currency(100, "USD"), "EUR"), currency(88, "EUR")
-    assert_equal currency_converter(codes).convert(currency(88, "EUR"), "USD"), currency(100,"USD")
+    assert_equal currency_converter(CODES).convert(currency(100, "USD"), "USD"), currency(100, "USD")
+    assert_equal currency_converter(CODES).convert(currency(100, "USD"), "EUR"), currency(88, "EUR")
+    assert_equal currency_converter(CODES).convert(currency(88, "EUR"), "USD"), currency(100,"USD")
   end
 
   def test_currency_convert_three
-    codes = {USD: 1.0, EUR: 0.88, GBP: 0.67}
-    assert_equal currency_converter(codes).convert(currency(100, "USD"), "EUR"), currency(88, "EUR")
-    assert_equal currency_converter(codes).convert(currency(88, "EUR"), "USD"), currency(100, "USD")
-    assert_equal currency_converter(codes).convert(currency(100, "USD"), "GBP"), currency(67, "GBP")
-    assert_equal currency_converter(codes).convert(currency(88, "EUR"), "GBP"), currency(67, "GBP")
+    assert_equal currency_converter(CODES).convert(currency(100, "USD"), "EUR"), currency(88, "EUR")
+    assert_equal currency_converter(CODES).convert(currency(88, "EUR"), "USD"), currency(100, "USD")
+    assert_equal currency_converter(CODES).convert(currency(100, "USD"), "GBP"), currency(67, "GBP")
+    assert_equal currency_converter(CODES).convert(currency(88, "EUR"), "GBP"), currency(67, "GBP")
   end
 
   def test_unknown_currency_code
-    codes = {USD: 1.0, EUR: 0.88, GBP: 0.67}
-    assert_raises(Unknown_Currency_Code) do currency_converter(codes).convert(currency(100,"USD"),"JPY") end
+    assert_raises(Unknown_Currency_Code) do currency_converter(CODES).convert(currency(100,"USD"),"JPY") end
   end
 end
