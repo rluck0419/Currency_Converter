19daf0d901976cc254687ae7f1a323da462e373f
Robert Luckfield <rluck0419@gmail.com>
2016-06-26 23:28:59 -0500

    updated second exchange rates for accurate readings as of sunday evening 6/26/16. JPY currently the best investment.

--- lib/currency_trader.rb
+++ lib/currency_trader.rb
@@ -7,13 +7,16 @@ class CurrencyTrader
 
   def best_investment(budget)
     investments = Hash.new
+
     @first_converter.codes.each_key do |code|
       investment = @first_converter.convert(Currency.new(budget, @starting_currency), code)
       investment = {investment.type => investment.amount}
       investments.merge!(investment)
     end
+
     greatest_profit = Currency.new(0, @starting_currency)
     type = @starting_currency
+
     investments.each do |code, amount|
       profit = @second_converter.convert(Currency.new(amount, code), @starting_currency)
       if profit.amount > greatest_profit.amount
@@ -21,8 +24,10 @@ class CurrencyTrader
         type = code.to_s
       end
     end
+
+    profit_amount = greatest_profit.amount - budget
     # Currency object:
-    greatest_profit.amount
-    type
+    "By investing $#{budget} in #{type}, you'd expect to get back $#{greatest_profit.amount}, with a profit of $#{profit_amount}."
+    # type
   end
 end
--- test/currency_trader_test.rb
+++ test/currency_trader_test.rb
@@ -8,7 +8,8 @@ class CurrencyTraderTest < MiniTest::Test
   # conversion rates at time 1
   CODES_ONE = {USD: 1.0, EUR: 0.88, GBP: 0.67, JPY: 102.20}
   # conversion rates at time 2
-  CODES_TWO = {USD: 1.0, EUR: 0.89, GBP: 0.72, JPY: 100.20}
+  CODES_TWO = {USD: 1.0, EUR: 0.91, GBP: 0.75, JPY: 101.91}
+  INVESTMENT = "By investing $1000000 in JPY, you'd expect to get back $1002846, with a profit of $2846."
 
   def currency_trader(codes_one, codes_two, starting_currency)
     ::CurrencyTrader.new(codes_one, codes_two, starting_currency)
@@ -39,6 +40,6 @@ class CurrencyTraderTest < MiniTest::Test
   def test_best_investment
     one = currency_converter(CODES_ONE)
     two = currency_converter(CODES_TWO)
-    assert_equal currency_trader(one, two, "USD").best_investment(1_000_000), "JPY"
+    assert_equal currency_trader(one, two, "USD").best_investment(1_000_000), INVESTMENT
   end
 end
